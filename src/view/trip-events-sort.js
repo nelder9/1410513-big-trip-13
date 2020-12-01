4e61bdba165281fd7ceaa537944c85943d688caf
nelder <nelder1990@gmail.com>
2020-12-01 22:36:29 +0300

    если эвентов нет - выводится сообщение, что нужно кликнуть на add event,добавил все три обработчика событий - на ESC, на стрелку вверх, на стрелку вниз

--- src/view/trip-events-sort.js
+++ src/view/trip-events-sort.js
@@ -1,6 +1,6 @@
 import {createElement} from "../utils.js";
 
-const createTripEventsTemplate = () => {
+const createTripEventsSortTemplate = () => {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
     <div class="trip-sort__item  trip-sort__item--day">
       <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
@@ -29,13 +29,13 @@ const createTripEventsTemplate = () => {
   </form>`;
 };
 
-export default class TripEvents {
+export default class TripEventsSort {
   constructor() {
     this._element = null;
   }
 
   getTemplate() {
-    return createTripEventsTemplate();
+    return createTripEventsSortTemplate();
   }
 
   getElement() {
