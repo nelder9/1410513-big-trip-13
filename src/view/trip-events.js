781c8c3be7bcf8121787ca2843e483ba70d9e4ba
nelder <nelder1990@gmail.com>
2020-12-01 21:55:31 +0300

    переделал функции на классы, добавил обработчик событий на эванты

--- src/view/trip-events.js
+++ src/view/trip-events.js
@@ -1,4 +1,6 @@
-export const createTripEventsTemplate = () => {
+import {createElement} from "../utils.js";
+
+const createTripEventsTemplate = () => {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
     <div class="trip-sort__item  trip-sort__item--day">
       <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
@@ -26,3 +28,26 @@ export const createTripEventsTemplate = () => {
     </div>
   </form>`;
 };
+
+export default class TripEvents {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripEventsTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
