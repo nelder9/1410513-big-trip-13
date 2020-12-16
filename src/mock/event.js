5be0481316578963c7f91ae936e3606b33eaa5c7
nelder <nelder1990@gmail.com>
2020-12-16 11:33:44 +0300

    Добавил смарт класс, который перисовывает карточку эвента, теперь можно редактировать моки по цене, типу и описанию, с оферами не стал делать, очень долго и нудно заносить эту инфу, в следующий понедельник буду брать данные уже с сервера

--- src/mock/event.js
+++ src/mock/event.js
@@ -1,11 +1,89 @@
 import dayjs from "dayjs";
 
-import {getRandomInteger} from "../utils/common.js";
+import {
+  getRandomInteger
+} from "../utils/common.js";
+
 
 const generateId = () => Date.now() + Math.ceil(Math.random() * 10000, 10);
 
+const generateDestination = () => {
+
+  const cities = [
+    {
+      name: `Geneva`,
+      text: `Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.`
+    },
+    {
+      name: `Chamonix`,
+      text: `Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.`
+    },
+    {
+      name: `Amsterdam`,
+      text: `Amsterdam, city and port, western Netherlands, located on the IJsselmeer and connected to the North Sea. It is the capital and the principal commercial and financial centre of the Netherlands.`
+    }
+  ];
+
+  const randomIndex = getRandomInteger(0, cities.length - 1);
+
+  return cities[randomIndex];
+};
+
 const generateType = () => {
-  const types = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`, `Restaurant`];
+  const types = [
+    {
+      name: `taxi`,
+      offers: [{
+        title: `Order Uber`,
+        price: 20,
+      }, {
+        title: `Order Yandex.taxi`,
+        price: 15,
+      }]
+    },
+    {
+      name: `bus`,
+      offers: null
+    },
+    {
+      name: `flight`,
+      offers: [{
+        title: `Choose seats`,
+        price: 5
+      }, {
+        title: `VIP class`,
+        price: 80,
+      }]
+    },
+    {
+      name: `train`,
+      offers: null
+    },
+    {
+      name: `ship`,
+      offers: null
+    },
+    {
+      name: `drive`,
+      offers: null
+    },
+    {
+      name: `drive`,
+      offers: null
+    },
+    {
+      name: `check-in`,
+      offers: null
+    },
+    {
+      name: `sightseeing`,
+      offers: null
+    },
+    {
+      name: `restaurant`,
+      offers: null
+    }
+  ];
   const randomIndex = getRandomInteger(0, types.length - 1);
 
   return types[randomIndex];
@@ -15,69 +93,10 @@ const generateTime = () => {
   return getRandomInteger(5, 60);
 };
 
-const generateOffer = () => {
-
-
-  const offers = [{
-    title: `Add luggage`,
-    price: 30},
-  {
-    title: `Switch to comfort class`,
-    price: 100,
-  },
-  {
-    title: `Add meal`,
-    price: 15
-  },
-  {
-    title: `Choose seats`,
-    price: 5
-  },
-  {
-    title: `Travel by train`,
-    price: 40,
-  },
-  {
-    title: `Order Uber`,
-    price: 20,
-  }
-  ];
-
-  const randomIndex = getRandomInteger(0, offers.length - 1);
-
-  return offers[randomIndex];
-
-};
-
-const generatePicture = () => {
-
-  return `http://picsum.photos/248/152?r=${Math.random()}`;
-
-};
-
-const generateDestination = () => {
-  const cities = [
-    `Moscow`,
-    `London`,
-    `Samara`,
-    `Barselona`,
-    `Milan`,
-    `Berlin`,
-  ];
-
-  const randomIndex = getRandomInteger(0, cities.length - 1);
-
-  return cities[randomIndex];
-
-};
 
 const generatePrice = () => {
 
   return getRandomInteger(10, 200);
-};
-
-const generateText = () => {
-  return `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 };
 
 export const generateEvent = () => {
@@ -92,11 +111,8 @@ export const generateEvent = () => {
     date: generateDate(),
     type: generateType(),
     time: generateTime(),
-    offer: generateOffer(),
     price: generatePrice(),
-    picture: generatePicture(),
     destination: generateDestination(),
-    text: generateText(),
     isFavorite: Boolean(getRandomInteger(0, 1))
   };
 };
--- src/view/trip-item.js
+++ src/view/trip-item.js
@@ -1,27 +1,40 @@
 import AbstractView from "./abstract.js";
 
+const createOfferTemplate = (type) => {
+  if (type.offers) {
+    return `<span class="event__offer-title">${type.offers[1].title}</span>
+    &plus;&euro;&nbsp;
+    <span class="event__offer-price">${type.offers[1].price}</span>`;
+  } else {
+    return ``;
+  }
+};
+
+
 const createTripItemTemplate = (event) => {
+
   const {
     date,
     type,
-    offer,
     price,
     destination,
     isFavorite,
     time
   } = event;
 
-  const favoriteClassName = isFavorite
-    ? `event__favorite-btn--active`
-    : ``;
+  const favoriteClassName = isFavorite ?
+    `event__favorite-btn--active` :
+    ``;
+
+  const offerTemplate = createOfferTemplate(type);
 
   return `<li class="trip-events__item">
     <div class="event">
       <time class="event__date" datetime="2019-03-18">${date}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.name}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">${type} ${destination}</h3>
+      <h3 class="event__title">${type.name} ${destination.name}</h3>
       <div class="event__schedule">
         <p class="event__time">
           <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
@@ -36,9 +49,7 @@ const createTripItemTemplate = (event) => {
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
         <li class="event__offer">
-          <span class="event__offer-title">${offer.title}</span>
-          &plus;&euro;&nbsp;
-          <span class="event__offer-price">${offer.price}</span>
+        ${offerTemplate}
         </li>
       </ul>
       <button class="event__favorite-btn ${favoriteClassName}" type="button">
